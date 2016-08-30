07d17ad7e529a87da0b9fc35ce5912b861d68a5b
davidefdl <davidefdl@yahoo.com>
2016-08-30 16:49:37 -0700

    Add test for log buf

--- tests/python/test_bpf_log.py
+++ tests/python/test_bpf_log.py
@@ -10,52 +10,58 @@ import os
 from unittest import main, TestCase
 
 
-#arg1 = sys.argv.pop(1)
 output = os.path.join(os.getcwd(), 'output.txt')
 sys.stderr = open(output, 'wb')
 error_msg = "R0 invalid mem access 'map_value_or_null'\n"
-text = ""
+
+text = """
+       #include <uapi/linux/ptrace.h>
+       #include <bcc/proto.h>
+       BPF_TABLE("hash", int, int, t1, 10);
+       int sim_port(struct __sk_buff *skb) {
+           int x = 0, *y;
+       """
+repeat = """
+           y = t1.lookup(&x);
+           if (!y) return 0;
+           x = *y;
+         """
+end = """
+           y = t1.lookup(&x);
+           x = *y;
+           return 0; 
+        }
+      """
+for i in range(0,300):
+    text += repeat
+text += end
 
 class TestBPFProgLoad(TestCase):
-    def setUp(self):
-        global text
-        text += """
-               #include <uapi/linux/ptrace.h>
-               #include <bcc/proto.h>
-               BPF_TABLE("hash", int, int, t1, 10);
-               int sim_port(struct __sk_buff *skb) {
-               int x = 0, *y;
 
-               """
-        repeat = """
-               y = t1.lookup(&x);
-               if (!y) return 0;
-               x = *y;
-               """
-        end = """
-               y = t1.lookup(&x);
-               x = *y;
-               return 0; 
-               }
-              """
-        for i in range(0,300):
-            text += repeat
-        text += end
-        #print(text)
-        #self.b = BPF(text=text, debug=2)
-
-    def test_jumps(self):
-        #print(text)
+    def test_log_debug(self):
         b = BPF(text=text, debug=2)
-
         try:
             ingress = b.load_func("sim_port",BPF.SCHED_CLS)
         except Exception:
             sys.stderr.flush()
             with open(output, 'rb') as f:
                 lines = f.readlines()
-                os.remove(output)
                 self.assertEqual(error_msg in lines, True)
+
+
+    def test_log_no_debug(self):
+        b = BPF(text=text, debug=0)
+        try:
+            ingress = b.load_func("sim_port",BPF.SCHED_CLS)
+        except Exception:
+            sys.stderr.flush()
+            with open(output, 'rb') as f:
+                lines = f.readlines()
+                self.assertEqual(error_msg in lines, True)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(output)
 
 if __name__ == "__main__":
     main()
